2af5a665718286b342310b08216f61d79f051c02
Rowena C. Calimag <rccalimag@headoffice.corp.bpi.com.ph>
2025-12-02 17:02:47 +0800

    first commit

---
+++ activity3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class activity3 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter first integer: ");
+		int intVal1 = input.nextInt();
+		System.out.println("Enter second integer: ");
+		int intVal2 = input.nextInt();
+		
+		
+		int sum = intVal1 + intVal2;
+		int diff = intVal1 - intVal2;
+		int product = intVal1 * intVal2;		
+		
+		System.out.println("sum: " + sum);
+		System.out.println("diff: " + diff);
+		System.out.println("product: " + product);
+
+		input.close();
+	}
+
+}
