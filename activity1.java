4ade35410410843eaf2440d8ccf57aa9c881bf8d
wengcalimag <rccalimag21@gmail.com>
2025-12-03 18:08:11 +0800

    Add files via upload

---
+++ activity1.java
@@ -0,0 +1,17 @@
+package com.bpi.individual;
+
+import java.util.Scanner;
+
+public class activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your name? ");
+		String name = input.next();
+		System.out.println("Hello, " + name + "!");
+
+		input.close();
+	}
+
+}
