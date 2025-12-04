2b74d63b9d41407b442faf06784db33cfdc4b279
Rowena C. Calimag <rccalimag@headoffice.corp.bpi.com.ph>
2025-12-04 20:47:41 +0800

    initial commit

---
+++ Car.java
@@ -0,0 +1,37 @@
+package com.bpi.individual;
+
+
+public class Car {
+
+	public static void main(String[] args) {
+		
+		String brandofCar = "| **Ford**          |";
+        String carType1 = " Focus              |";
+        String carType2 = " Everest, Explorer |";
+		
+        System.out.println("**Some popular car brands and the types of cars they offer  **");
+        System.out.println("**============================================================");
+		System.out.println("| **Brand**         | **Sedan**          | **SUV**           |");
+		System.out.println("| ----------------- | ------------------ | ----------------- |" );
+		
+        //for no args constructor
+        Car1 noArgCar = new Car1();
+        noArgCar.setCarBrand(brandofCar);
+        noArgCar.setCarType1(carType1);
+        noArgCar.setCarType2(carType2);
+        noArgCar.getfirstCar();
+        
+        
+        //for parameterized constructor
+		Car1 paramCar = new Car1("| **Toyota**        |", " Camry, Corolla     |", " Fortuner, RAV4    |");
+		paramCar.displayCar();
+		
+		paramCar.setCarBrand("| **Tesla**         |");
+		paramCar.setCarType1(" Model 3, Model S   |");
+		paramCar.setCarType2(" Model X, Model Y  |");
+		paramCar.displayCar();
+		
+       
+	}
+
+}
---
+++ Car1.java
@@ -0,0 +1,53 @@
+package com.bpi.individual;
+
+public class Car1 {
+
+	String carBrand;
+    String carType1;
+	String carType2;
+
+	//no-args Constructor
+	public Car1 () {	
+	}
+	
+	public void getfirstCar() {
+		System.out.println(this.carBrand + this.carType1 + this.carType2);
+	}
+	
+	public Car1(String carBrand, String carType1, String carType2) {
+      this.carBrand = carBrand;
+      this.carType1 = carType1;
+      this.carType2 = carType2;
+	}
+	
+	public void displayCar () {
+		System.out.println(this.carBrand + this.carType1 + this.carType2);
+
+	}
+
+	public String getCarBrand() {
+		return carBrand;
+	}
+
+	public void setCarBrand(String carBrand) {
+		this.carBrand = carBrand;
+	}
+
+	public String getCarType1() {
+		return carType1;
+	}
+
+	public void setCarType1(String carType1) {
+		this.carType1 = carType1;
+	}
+
+	public String getCarType2() {
+		return carType2;
+	}
+
+	public void setCarType2(String carType2) {
+		this.carType2 = carType2;
+	}
+	
+
+}
